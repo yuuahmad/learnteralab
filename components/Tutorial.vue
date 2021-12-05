9d25843c050b00ca17d17c18d72a4d1127292254
yuuahmad <ahmadyusufmaulana0@gmail.com>
2021-12-06 00:38:54 +0700

    commit pertama saya

---
+++ components/Tutorial.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="text-xl font-semibold text-center capitalize">
+    ini adalah kata pertama saya dalam palajaran nuxt kali ini
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
